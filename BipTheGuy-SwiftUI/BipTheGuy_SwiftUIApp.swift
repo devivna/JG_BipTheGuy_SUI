3a4655abf756916f47ee3312e900083b873121d9
Nadia <ivashchenko.nadia@gmail.com>
2023-02-03 12:41:40 +0200

    Initial Commit

---
+++ BipTheGuy-SwiftUI/BipTheGuy_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuy_SwiftUIApp.swift
+//  BipTheGuy-SwiftUI
+//
+//  Created by Students on 03.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuy_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
