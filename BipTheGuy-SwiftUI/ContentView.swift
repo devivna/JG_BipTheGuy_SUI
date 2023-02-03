d6df1dbf5dc616948a5d07dd1681957fe16ec6f3
Nadia <ivashchenko.nadia@gmail.com>
2023-02-03 15:00:52 +0200

    3.2 sound + scale effect

--- BipTheGuy-SwiftUI/ContentView.swift
+++ BipTheGuy-SwiftUI/ContentView.swift
@@ -6,17 +6,56 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var imageEffect = true
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            Image("clown")
+                .resizable()
+                .scaledToFit()
+                .scaleEffect(imageEffect ? 1.0 : 0.9)
+                .onTapGesture {
+                    playSound(soundName: "punchSound")
+                    imageEffect = false
+                    withAnimation(.spring(response: 0.3, dampingFraction: 0.3)) {
+                        imageEffect = true
+                    }
+                    
+                }
+            
+            Spacer()
+            
+            Button {
+                //
+            } label: {
+                Label("Photo Library", systemImage: "photo.tv")
+            }
+
         }
         .padding()
     }
+    
+    func playSound(soundName: String) {
+        
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("Couldn't read file named \(soundName)")
+            return
+        }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("Error: \(error.localizedDescription) creating audioPlayer.")
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
